58c5185becb037aa61ef36c2fa7c56c5f79a1cd9
Jean Jaque <jean.jaque@gmail.com>
2022-02-06 12:25:46 -0300

    first commit

---
+++ assets/js/jar.js
@@ -0,0 +1,7 @@
+const texto1 ="hola";
+const texto2 ="desafio";
+const texto3 ="latam";
+
+const saludo = texto1 + " " + texto2 + " " + texto3;
+const saludointerpolado =`${texto1} ${texto2} ${texto3}`;
+console.log(saludointerpolado);
